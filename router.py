0b04b6e0f1485f633ea4bc488efa04305d8470ba
Pedro Cunha <104006553+nevidomyyb@users.noreply.github.com>
2023-04-03 16:11:24 -0300

    Update router.py

--- router.py
+++ router.py
@@ -1,5 +1,5 @@
 def select_database(database):
 
-    if database == "alternative": return "postgresql://postgres:Pedro123.321@localhost/flask1"
-    if database == "default": return "postgresql://postgres:Pedro123.321@localhost/flask"
+    if database == "alternative": return "postgresql://postgres:password@localhost/flask1"
+    if database == "default": return "postgresql://postgres:password@localhost/flask"
     raise Exception("invalid database router")
